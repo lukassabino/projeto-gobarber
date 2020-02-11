b75ce9a330bf4bf2f0a87f2bd20746bd2959b13f
Lukas <lukassabino_@hotmail.com>
2020-02-10 21:59:09 -0300

    Adicionado mecanismo de cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -25,10 +25,13 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
