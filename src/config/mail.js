92b7840a0994b397357678bff58efd5cc9605adc
Lukas <lukassabino_@hotmail.com>
2020-03-01 12:07:37 -0300

    Criação de variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ab8f2977cec364',
-    pass: '22d8f000968ce9',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://5f3d746028b548e99b10b0cdf6251fc1@sentry.io/3355008',
+  dsn: process.env.SENTRY_DSN,
+  // dsn: 'https://5f3d746028b548e99b10b0cdf6251fc1@sentry.io/3355008',
 };
